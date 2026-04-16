a19bc86642556b767b04ebfb26bea0ff23001e57
Matheus Arthur <matheus.artsilva@gmail.com>
2026-04-15 22:51:48 -0300

    feat(p1): inicia projeto
    - Cria componente principal
    - Instala e importa Bootstrap

---
+++ p1_pdm/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-12">
+
+          <h4>Hello, Investimentos</h4>
+          <p className="text-muted">
+            Descubra quanto seu dinheiro pode render com juros compostos
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
